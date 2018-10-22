72e547f1758952921889113125418d4b1aec84ca
MidouDev <htmidou@gmail.com>
2018-10-22 14:54:06 +0100

    Creation du header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGhost } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faGhost)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="container">
+            <div className="row m-auto">
+              <i className="fa fa-hand-pointer-o fa-4x text-white"></i>
+              <div className="h1 ml-2 my-auto text-light">React Clicker</div>
+            </div>
+          </div>
         </header>
+
+        <div>
+          
+        </div>
+        
       </div>
     );
   }
