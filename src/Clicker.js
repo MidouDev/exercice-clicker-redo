7a95d05baf9f0e76a17e8b0aad8754ca168203d0
MidouDev <htmidou@gmail.com>
2018-10-25 14:09:54 +0100

    fixing the clicker width

--- src/Clicker.js
+++ src/Clicker.js
@@ -2,23 +2,25 @@ import React from 'react';
 import './Clicker.css';
 
 const Clicker = ({counter,onPlusClick,onRefreshClick,onMinusClick}) => (
-    <div className="clickerContainer">
-        <div className="screen">
-            <span>{counter}</span>
-        </div>
-        <div className="controllers">
-            <button className="controller plus" onClick={onPlusClick}>
-                <span>+</span>
-            </button>
-            <button className="controller refresh" onClick={onRefreshClick}>
-                <i className="fa fa-refresh fa-2x"></i>
-            </button>
-            <button className="controller minus" onClick={onMinusClick}>
-                <span>-</span>
-            </button>
-            
+    <div className="container">
+        <div className="clickerContainer">
+            <div className="screen d-flex">
+                <div>{counter}</div>
+            </div>
+            <div className="controllers">
+                <button className="controller plus" onClick={onPlusClick}>
+                    <i className="fa fa-plus fa-2x text-white"></i>
+                </button>
+                <button className="controller refresh" onClick={onRefreshClick}>
+                    <i className="fa fa-refresh fa-2x"></i>
+                </button>
+                <button className="controller minus" onClick={onMinusClick}>
+                    <i className="fa fa-minus fa-2x text-white"></i>
+                </button>
+            </div>
         </div>
     </div>
+    
 );
 
 export default Clicker;
