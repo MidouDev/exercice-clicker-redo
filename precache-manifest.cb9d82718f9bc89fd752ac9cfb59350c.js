c1b9883191d299f57521b1f9fea3969ecf512b0e
MidouDev <htmidou@gmail.com>
2018-10-26 00:54:32 +0100

    Updates

---
+++ precache-manifest.cb9d82718f9bc89fd752ac9cfb59350c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/exercice-clicker-redo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "cf2f57c1977c50287d18",
+    "url": "/exercice-clicker-redo/static/css/main.7de2c402.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/exercice-clicker-redo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "593d79b2170e1f0c5f6e",
+    "url": "/exercice-clicker-redo/static/js/1.593d79b2.chunk.js"
+  },
+  {
+    "revision": "41b16dbddc1c507c134f",
+    "url": "/exercice-clicker-redo/static/js/runtime~main.41b16dbd.js"
+  },
+  {
+    "revision": "cf2f57c1977c50287d18",
+    "url": "/exercice-clicker-redo/static/js/main.cf2f57c1.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/exercice-clicker-redo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/exercice-clicker-redo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/exercice-clicker-redo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "593d79b2170e1f0c5f6e",
+    "url": "/exercice-clicker-redo/static/css/1.b587b8ef.chunk.css"
+  },
+  {
+    "revision": "e3e94328b931db738be9000e50e5bb69",
+    "url": "/exercice-clicker-redo/index.html"
+  }
+];
